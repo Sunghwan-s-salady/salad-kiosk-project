1439ef566ce07d2d0e4f9fc6bd7283928b1ed60d
parfait2 <parkyl0208@naver.com>
2023-04-05 18:33:07 +0900

    test complete

---
+++ src/test/Test.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.List;
+
+import controller.CustomerController;
+import controller.ManagerController;
+import model.dao.CustomerDAO;
+import model.dao.CustomerDAOImpl;
+import model.dao.ManagerDAO;
+import model.dao.ManagerDAOImpl;
+import model.dto.MenuDTO;
+import model.dto.OrderDTO;
+import view.MenuView;
+
+public class Test {
+
+
+	public static void main(String[] args)  {
+
+	}
+		
+	// #1
+	static void insert(){
+
+	}
+	
+}
