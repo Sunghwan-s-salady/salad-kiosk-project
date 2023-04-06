dc0f34816c5c969c978444af102b91d16c1f3e83
parfait2 <parkyl0208@naver.com>
2023-04-06 09:52:38 +0900

    add CustomerController comment

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -19,10 +19,11 @@ public class CustomerController {
 	public static List<MenuDTO> mainList = new ArrayList<>();
 	public static List<MenuDTO> toppList = new ArrayList<>();
 	public static List<MenuDTO> sideList = new ArrayList<>();
-   /**
-    * @author 박예린
-    * 
-    * */
+	/**
+	 * @author 박예린
+	 * 메인 메뉴를 선택하는 controller이다.
+	 * DAO를 통해 처리한 후 view로 보여준다.
+	 */
 	public static void selectMainMenu() throws SearchWrongException {
         try {
         	mainList = customerDAO.selectMainMenu();
@@ -31,7 +32,11 @@ public class CustomerController {
 			   FailView.errorMessage(e.getMessage());
 	   }
     }
-	
+	/**
+	 * @author 박예린
+	 * 토핑을 선택하는 controller이다.
+	 * DAO를 통해 처리한 후 view로 보여준다.
+	 */
 	public static void selectTopping() throws SearchWrongException {
         try {
         	toppList = customerDAO.selectTopping();
@@ -41,7 +46,11 @@ public class CustomerController {
 			   FailView.errorMessage(e.getMessage());
 	   }
     }
-	
+	/**
+	 * @author 박예린
+	 * 사이드 메뉴를 선택하는 controller이다.
+	 * DAO를 통해 처리한 후 view로 보여준다.
+	 */
 	public static void selectSideMenu() throws SearchWrongException {
         try {
         	sideList = customerDAO.selectSide();
@@ -50,43 +59,36 @@ public class CustomerController {
 			   FailView.errorMessage(e.getMessage());
 	   }
     }
-	
-   /**
-    * @author 박예린
-    * 메인 메뉴, 토핑 추가, 사이드 메뉴 선택 후 결제 전 고객의 주문 내역을 보여준다.
-    * */
-	
 	/**
-    * @author 박예린
-    * 메인 메뉴, 토핑 추가, 사이드 메뉴 선택 후 OrderDTO에 담는다.
-    * insertOrdersList
-    * */
+	 * @author 박예린
+	 * @return result
+	 * 메인 메뉴, 토핑 추가, 사이드 메뉴 선택 후 OrderDTO에 담는다.
+	 * view에서 TOTAL_AMOUNT, EAT_HOW를 받아오고, 나머지는 DAO에서 처리한다.
+	 * DAO 처리 성공 여부를 result로 리턴한다.
+	 */
 	public static int insertOrdersList() throws SearchWrongException {
-		int result = 0;
-		
 		OrderDTO order = new OrderDTO(MenuView.cost, MenuView.place);
-		customerDAO.insertOrdersList(order);
+		int result = customerDAO.insertOrdersList(order);
 
         return result;
     }
 	
 	/**
-    * @author 박예린
-    * 메인 메뉴, 토핑 추가, 사이드 메뉴 선택 후 OrderDetailDTO orderDetail에 담는다.
-    * insertDetailList
-    * */
+	 * @author 박예린
+	 * @return result
+	 * view에서 purchase_List를 받아와서 상품 코드와 수량을 OrderDetailDTO에 담는다.
+	 * view에서 TOTAL_AMOUNT를 받아와서 OrderDetailDTO에 담는다. 나머지는 DAO에서 처리한다.
+	 * DAO 처리 성공 여부를 result로 리턴한다.
+	 */
 	public static int orderDetailInsert() throws SearchWrongException {
 		int result = 0;
-		
 		MenuDTO menuDTO = new MenuDTO();
-		// 이 놈 안에 MenuDTO가 있어 => 이거로  OrderDetailDTO를 만들어 
 		
 		for(MenuDTO me : MenuView.purchase_List) {
-			OrderDetailDTO orderDetail = new OrderDetailDTO(me.getProductCode(), menuDTO.getCount(), MenuView.cost); // orderCount 수정해야함
-			// 만든 거를 insert
-			customerDAO.insertDetailList(orderDetail);
+			OrderDetailDTO orderDetail = new OrderDetailDTO(me.getProductCode(), menuDTO.getCount(), MenuView.cost);
+			result = customerDAO.insertDetailList(orderDetail);
 		}
-
+		
         return result;
     }
 }
